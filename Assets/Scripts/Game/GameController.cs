67d84c5f80f950bb693975bf9da6c12cb450b719
Хазиев Рамис <hazievrr@sibintek.ru>
2021-05-29 14:16:02 +0400

    Game scene and basic objects

---
+++ Assets/Scripts/Game/GameController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class GameController : MonoBehaviour
+    {
+        [SerializeField] private Player m_playerOne;
+        [SerializeField] private Player m_playerTwo;
+        [SerializeField] private Ball m_ball;
+        
+        
+    }
+}
---
+++ Assets/Scripts/Game/Player.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Game
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private int m_index;
+
+        public void Move(float x)
+        {
+            
+        }
+    }
+}
